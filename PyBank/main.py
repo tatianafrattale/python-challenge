16dd2f09f48253a3179702ae39d284f46bce9e04
Tatiana Frattale <84927172+tatianafrattale@users.noreply.github.com>
2021-07-07 00:38:40 -0400

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,83 +1,73 @@
-# Challenge 1- PyBank
+# You will be give a set of poll data called election_data.csv. The dataset is composed of three columns: Voter ID, County, and Candidate. Your task is to create a Python script that analyzes the votes and calculates each of the following:
+# The total number of votes cast
+# A complete list of candidates who received votes
+# The percentage of votes each candidate won
+# The total number of votes each candidate won
+# The winner of the election based on popular vote.
 
-# Your task is to create a Python script that analyzes the records to calculate each of the following:
-# The total number of months included in the dataset
-# The net total amount of "Profit/Losses" over the entire period
-# The average of the changes in "Profit/Losses" over the entire period
-# The greatest increase in profits (date and amount) over the entire period
-# The greatest decrease in losses (date and amount) over the entire period
 
-# Import os and csv
+# Import modules
 import os
 import csv
- 
-# Set path for csv
-pybankcsv = os.path.join("Resources", "pybank_budget_data.csv")
 
-# Create variables and lists
-total_months = []
-net_profit_loss = []
-month_to_month = []
+# Set path for csv 
+pypollcsv = os.path.join("Resources", "election_data.csv")
 
-# Variable counts
-month_count = 0
-total_profit = 0
+# Variable counters
+total_votes = 0
+total_correy= 0
+total_khan = 0
+total_li = 0
+total_otooley = 0
 
-
-# Open csv
-with open(pybankcsv, newline="") as csvfile:
+# Open csv and skip header
+with open(pypollcsv, newline="") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csv_reader)
 
-for row in csv_reader:
-    # Count months
-    month_count = month_count + 1
+# Read each row
+for row in csv_reader:  
+    # Total number of votes, just found out about the += function, what a time saver!!!
+    total_votes += 1
 
-    # Add to total to months list
-    total_months.append(row[0])
+    # Total votes per candidate
+    if (row[2] == "Correy"):
+        total_correy += 1
+    elif (row[2] == "Khan"):
+        total_khan += 1
+    elif (row[2] == "Li"):
+        total_li += 1
+    elif (row[2] == "O'Tooley"):
+        total_otooley += 1
 
-    # Net total amount of Profit and losses over entire period
-    total_profit = total_profit + int(row[1])
+    # Percentage votes per candidate
+    correy_percent_vote = total_correy / total_votes
+    khan_percent_vote = total_khan / total_votes
+    li_percent_vote = total_li / total_votes
+    otooley_percent_vote = total_otooley / total_votes
 
-    # Add total profit to net profit loss list
-    net_profit_loss.append(row[1])
+    # Format into percentage
+    correy_percentage = "{:.0%}".format(correy_percent_vote)
+    khan_percentage = "{:.0%}".format(khan_percent_vote)    
+    li_percentage = "{:.0%}".format(li_percent_vote)
+    otooley_percentage = "{:.0%}".format(otooley_percent_vote)
 
-    # Calculate average of changes in profit and losses over entire period. I need to take the total profit/loss and divide it by the amount of months
-    average_change_profit = (total_profit / month_count)
+    # Winner based on popular vote
+    if total_correy > total_khan and total_li and total_otooley:
+        winner = "Correy"
+    elif total_khan > total_correy and total_li and total_otooley:
+        winner = "Khan"
+    elif total_li > total_correy and total_khan and total_otooley:
+        winner = "Li"
+    elif total_otooley > total_correy and total_khan and total_li:
+        winner = "O'Tooley"
 
-    # In order to find the greatest increase and decrease in profit, I need to calculate the changes from month to month? Store those values?
-    current_month_profit = int(row[1])
-    previous_month_profit = int(row[1]) - current_month_profit
-    month_to_month = current_month_profit - previous_month_profit
-    
-    # The greatest increase in profits over the entire period, using max function, 
-    greatest_increase = max(month_to_month)
-
-    # The greatest decrease in losses over the entire period, using min function, 
-    greatest_decrease = min(month_to_month)
-
-    # Find the date for the greatest increase
-    greatest_increase_date = [month_to_month.index(greatest_increase)]
-
-    #Find the date for the greatest decrease
-    greatest_decrease_date = [month_to_month.index(greatest_decrease)]
-
-# Create the print statement
-print("Financial Analysis")
-print("---------------------------------------------")
-print("Total Months: " + str(month_count))
-print("Total Profit: " + str(total_profit))
-print("Average Change: " + "$" + str(average_change_profit)
-print("Greatest Increase in Profits: " + str(greatest_increase_date) + str(greatest_increase))
-print("Greatest Decrease in Profits: " + str(greatest_decrease_date) + str(greatest_decrease))
-
-# Open the text file
-
-with open('analysis.txt, 'w') as text:
-    text.write("---------------------------------------\n")
-    text.write("Financial Analysis" + "\n")
-    text.write("Total Months: " + str(month_count) + "\n")
-    text.write("Total Profit: " + "$" + str(total_profit) +"\n")
-    text.write("Average Change: " + "$" + str(average_change_profit) + "\n")
-    text.write("Greatest Increase In Profit: " + str(greatest_increase_date) + "$" + str(greatest_increase) + "\n")
-    text.write("Greatest Decrease In Profit: " + str(greatest_decrease_date) + "$" + str(greatest_decrease) + "\n")
+# Print Statement in integrated terminal
+print("Election Results")  
+print("--------------------------------------")
+print("Total Votes: " + str(total_votes)) 
+print("--------------------------------------")
+print("Correy: " + correy_percentage + str(total_correy))
+print("Khan: " + khan_percentage + str(total_khan))
+print("Li: " + li_percentage + str(total_li))
+print("O'Tooley: " + otooley_percentage + str(total_otooley))
